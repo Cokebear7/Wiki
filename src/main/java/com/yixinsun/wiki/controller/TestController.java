6e09efb09d91e6c429fd710591e7c99ddd79b21b
sunyixin <syxcoco7@gmail.com>
2021-10-18 00:45:51 +0800

    2-7 add hello interface test: test.http

--- src/main/java/com/yixinsun/wiki/controller/TestController.java
+++ src/main/java/com/yixinsun/wiki/controller/TestController.java
@@ -21,5 +21,10 @@ public class TestController {
     public String hello() {
         return "Hello World!";
     }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post，" + name;
+    }
 }
 
