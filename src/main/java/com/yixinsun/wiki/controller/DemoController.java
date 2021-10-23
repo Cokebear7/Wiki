195a720a23cf416b1fd7867d25b65aaf95cd9baf
sunyixin <syxcoco7@gmail.com>
2021-10-23 18:36:43 +0800

    3-5 add demo table query

---
+++ src/main/java/com/yixinsun/wiki/controller/DemoController.java
@@ -0,0 +1,26 @@
+package com.yixinsun.wiki.controller;
+
+import com.yixinsun.wiki.domain.Demo;
+import com.yixinsun.wiki.service.DemoService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Resource
+    private DemoService demoService;
+
+    @GetMapping("/list")
+    public List<Demo> list() {
+        return demoService.list();
+    }
+}
+
---
+++ src/main/java/com/yixinsun/wiki/service/DemoService.java
@@ -0,0 +1,20 @@
+package com.yixinsun.wiki.service;
+
+import com.yixinsun.wiki.domain.Demo;
+import com.yixinsun.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list() {
+        return demoMapper.selectByExample(null);
+    }
+}
+
