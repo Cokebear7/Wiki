5ce2e1b2c6872dd951f4357f3a3c828a227dfaec
sunyixin <syxcoco7@gmail.com>
2021-10-25 00:36:43 +0800

    3-8 encapsulate BeanUtils as CopyUtil

--- src/main/java/com/yixinsun/wiki/service/EbookService.java
+++ src/main/java/com/yixinsun/wiki/service/EbookService.java
@@ -5,6 +5,7 @@ import com.yixinsun.wiki.domain.EbookExample;
 import com.yixinsun.wiki.mapper.EbookMapper;
 import com.yixinsun.wiki.req.EbookReq;
 import com.yixinsun.wiki.resp.EbookResp;
+import com.yixinsun.wiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -24,14 +25,20 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
 
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
+        // List<EbookResp> respList = new ArrayList<>();
+        // for (Ebook ebook : ebookList) {
+        //     // EbookResp ebookResp = new EbookResp();
+        //     // BeanUtils.copyProperties(ebook, ebookResp);
+        //     // 对象复制
+        //     EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+        //
+        //     respList.add(ebookResp);
+        // }
 
-        return respList;
+        // 列表复制
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+
+        return list;
     }
 }
 
