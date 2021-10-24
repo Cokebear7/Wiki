27e55c044c831c96ed3e683f6c67c8c127b138b2
sunyixin <syxcoco7@gmail.com>
2021-10-25 00:11:24 +0800

    3-6 add the common return class CommonResp, and the return values of all back-end interfaces in the future will all be of CommonResp type

--- src/main/java/com/yixinsun/wiki/controller/EbookController.java
+++ src/main/java/com/yixinsun/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.yixinsun.wiki.controller;
 
 import com.yixinsun.wiki.domain.Ebook;
+import com.yixinsun.wiki.resp.CommonResp;
 import com.yixinsun.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
 
