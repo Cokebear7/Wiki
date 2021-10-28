f5759bc5947801c91e7f903bbfaf5b3bb3ef1ca7
sunyixin <syxcoco7@gmail.com>
2021-10-28 23:24:25 +0800

    6-5 Encapsulate page request parameter PageReq and return parameter PageResp

--- src/main/java/com/yixinsun/wiki/controller/EbookController.java
+++ src/main/java/com/yixinsun/wiki/controller/EbookController.java
@@ -1,16 +1,15 @@
 package com.yixinsun.wiki.controller;
 
-import com.yixinsun.wiki.domain.Ebook;
 import com.yixinsun.wiki.req.EbookReq;
 import com.yixinsun.wiki.resp.CommonResp;
 import com.yixinsun.wiki.resp.EbookResp;
+import com.yixinsun.wiki.resp.PageResp;
 import com.yixinsun.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @RestController
 @RequestMapping("/ebook")
@@ -21,10 +20,9 @@ public class EbookController {
 
     @GetMapping("/list")
     public CommonResp list(EbookReq req) {
-        CommonResp<List<EbookResp>> resp = new CommonResp<>();
-        List<EbookResp> list = ebookService.list(req);
+        CommonResp<PageResp<EbookResp>> resp = new CommonResp<>();
+        PageResp<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
 }
-
