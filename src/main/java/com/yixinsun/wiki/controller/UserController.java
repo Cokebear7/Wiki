b35fd3083463b3b74529dd580778df4ec2a073e0
sunyixin <syxcoco7@gmail.com>
2021-12-07 23:29:11 +0800

    9-3 Complete the basic addition, deletion, modification, and query function of the user table

---
+++ src/main/java/com/yixinsun/wiki/controller/UserController.java
@@ -0,0 +1,42 @@
+package com.yixinsun.wiki.controller;
+
+import com.yixinsun.wiki.req.UserQueryReq;
+import com.yixinsun.wiki.req.UserSaveReq;
+import com.yixinsun.wiki.resp.CommonResp;
+import com.yixinsun.wiki.resp.UserQueryResp;
+import com.yixinsun.wiki.resp.PageResp;
+import com.yixinsun.wiki.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    @GetMapping("/list")
+    public CommonResp list(@Valid UserQueryReq req) {
+        CommonResp<PageResp<UserQueryResp>> resp = new CommonResp<>();
+        PageResp<UserQueryResp> list = userService.list(req);
+        resp.setContent(list);
+        return resp;
+    }
+
+    @PostMapping("/save")
+    public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        CommonResp resp = new CommonResp<>();
+        userService.save(req);
+        return resp;
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp delete(@PathVariable Long id) {
+        CommonResp resp = new CommonResp<>();
+        userService.delete(id);
+        return resp;
+    }
+}
