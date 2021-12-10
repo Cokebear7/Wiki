4a5f49f13d6c442297f33445802202a5a80d7aaa
Yixin Sun <95908307+yixinsun99@users.noreply.github.com>
2021-12-10 23:48:55 +0800

    9-5 Password encrypted transmission and encrypted storage

--- src/main/java/com/yixinsun/wiki/controller/UserController.java
+++ src/main/java/com/yixinsun/wiki/controller/UserController.java
@@ -3,9 +3,10 @@ package com.yixinsun.wiki.controller;
 import com.yixinsun.wiki.req.UserQueryReq;
 import com.yixinsun.wiki.req.UserSaveReq;
 import com.yixinsun.wiki.resp.CommonResp;
-import com.yixinsun.wiki.resp.UserQueryResp;
 import com.yixinsun.wiki.resp.PageResp;
+import com.yixinsun.wiki.resp.UserQueryResp;
 import com.yixinsun.wiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -28,6 +29,7 @@ public class UserController {
 
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req) {
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
