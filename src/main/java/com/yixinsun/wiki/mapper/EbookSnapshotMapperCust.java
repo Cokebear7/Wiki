cf7dc7c89e4b6c312e1c348582360d67f31ec761
sunyixin <43490884+Cokebear7@users.noreply.github.com>
2021-12-13 15:10:54 +0800

    11-6 Add an interface to the backend to obtain statistics on the wiki

--- src/main/java/com/yixinsun/wiki/mapper/EbookSnapshotMapperCust.java
+++ src/main/java/com/yixinsun/wiki/mapper/EbookSnapshotMapperCust.java
@@ -1,6 +1,12 @@
 package com.yixinsun.wiki.mapper;
 
+import com.yixinsun.wiki.resp.StatisticResp;
+
+import java.util.List;
+
 public interface EbookSnapshotMapperCust {
 
     public void genSnapshot();
+
+    List<StatisticResp> getStatistic();
 }
